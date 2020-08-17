8289e24464ec1deca01134c87b73208ec4cff2ca
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-08-17 11:50:58 +0300

    update calculator excercise

---
+++ W2/D2/normal/ex_1/script.js
@@ -0,0 +1,124 @@
+
+
+//JS CONDITIONALS 
+
+//Syntax
+
+/*
+
+if(condition){
+	
+	Task to perform
+
+} else if(){}
+
+else{}
+
+
+*/
+
+
+
+var number = 2;
+
+// if (number > 2) {
+
+// 	console.log('number if greater than 2');
+
+// } else if( number == 2 ){
+
+// 	console.log('number is equal to 2');
+
+// } else{
+
+// 	console.log('number is not greater than or equal to 2');
+
+// }
+
+
+
+// if (  !(number > 2)   ) {
+
+// 	console.log('The condition is met');
+
+// }
+
+
+
+var age = '18';
+
+// if (age > 18) {
+//     console.log("You're a big man")
+// }
+
+
+// if (age > 18) {
+
+//     console.log("We can go to a pub together !!")
+
+// } else {
+//      console.log("Sorry...You have to stay at home tonight")
+// }
+
+
+// if (age === 18) {
+
+//     console.log("It's your birthday !!");
+
+// } else if (age > 18) {
+
+//     console.log("We can go to a pub together !!");
+
+// } else {
+//     console.log("Sorry...You have to stay at home tonight")
+// }
+
+
+
+
+//SWITCH CONDITION
+
+// var favoriteColor = 'orange';
+
+// switch(favoriteColor){
+
+// 	case 'blue': console.log('Favorite color is blue');
+// 	break;
+
+// 	case 'red': console.log('The favorite color is red');
+// 	break;
+
+// 	case 'yellow': console.log('The favorite color is yellow');
+// 	break;
+
+// 	default: console.log('I dont know your favorite color');
+// 	break;
+
+// }
+
+
+
+
+// var expr = 'Papayas';
+
+// switch (expr) {
+
+//   case 'Oranges':
+
+//     console.log('Oranges are $0.59 a pound.');
+//     break;
+
+//   case 'Mangoes':
+
+//     console.log('Mangoes are $1.50 a pound.');
+//     break;
+
+//   case 'Papayas':
+
+//     console.log('Mangoes and papayas are $2.79 a pound.');
+//     // expected output: "Mangoes and papayas are $2.79 a pound."
+//     break;
+
+//   default:
+//     console.log('Sorry, we are out of ' + expr + '.');
+// }
