0c8fbae1e13f7b8b9fc2a7d9429ed748a2326f7c
roy4ik <67188481+roy4ik@users.noreply.github.com>
2020-08-17 14:45:32 +0300

    update

--- W2/D1/ninja/js/script.js
+++ W2/D1/ninja/js/script.js
@@ -28,23 +28,65 @@
 // console.log(splittedInput[0] * splittedInput[1]); //if input 45, 45 result 2025;
 
 // exercise 4;
-let boom = prompt('Enter the O number in Boom');
-let n = parseInt(boom);
-if (n.length < 0) {
-    let inputstr = "b" + "o".repeat(n) + "m";
-    if ((n % 5) === 0) {
-        inputstr = inputstr.toUpperCase();
-        if ((n % 2) === 0) {
-            inputstr = inputstr.concat("!");
+
+
+let n = 20
+
+function boomRoy(n) {
+    // let boom = prompt('Enter the O number in Boom');
+    // let n = parseInt(Math.round(boom));
+    if (n.length != 0) {
+        let inputstr = "b" + "o".repeat(n) + "m";
+        if ((n % 5) === 0) {
+            inputstr = inputstr.toUpperCase();
+            if ((n % 2) === 0) {
+                inputstr = inputstr.concat("!");
+            }
+            console.log(inputstr);
+        } else if ((n > 0) || ((n % 2) === 0)) {
+            if (n === 1) {
+                n = 2;
+                inputstr = "b" + "o".repeat(n) + "m";
+            }
+            console.log(inputstr);
         }
-        console.log(inputstr);
-    } else if ((n > 2) || ((n % 2) === 0)) {
-        console.log(inputstr);
     } else {
-        console.log("Looks like you didn't put a proper n, DUMBASS!!")
+        console.log("You didn't enter anything")
+        console.log("Error: " + errorMsg);
+        let errorMsg = -1;
     }
-} else {
-    console.log("You didn't enter anything")
-    let errorMsg = -1;
-    console.log("Error: " + errorMsg);
 }
+
+// function boomDaniel(n) {
+//     // var n = parseInt(prompt("Enter a number of 'O' you want"));
+
+//     if (n === 1 || n === 2) {
+//         console.log("boom");
+//     } else if ((n % 2 === 0) && (n % 5 === 0)) {
+//         var x = "B" + ("o".repeat(n)) + "m " + "!";
+//         console.log(x.toUpperCase());
+//     } else if (n % 2 === 0) {
+//         var x = "B" + ("o".repeat(n)) + "m " + "!";
+//         console.log(x);
+//     } else if (n % 5 === 0) {
+//         var x = "B" + ("o".repeat(n)) + "m ";
+//         console.log(x.toUpperCase());
+//     } else if (n > 2) {
+//         var x = "B" + ("o".repeat(n)) + "m ";
+//         console.log(x);
+//     } else {
+//         console.log("Start Over MANIAK !");
+//     }
+// }
+
+// // function callRoyfunc() {
+// //     for (n = 0; n <= 2000; ++n) {
+// //         boomRoy(n);
+// //     }
+// // }
+
+// // function callDanielfunc() {
+// //     for (n = 0; n <= 2000; ++n) {
+// //         boomDaniel(n);
+// //     }
+// // }
