c64e55736ece8066d86f8a8bce3bb3284281c4b1
jcohenp <50540306+jcohenp@users.noreply.github.com>
2020-09-07 20:17:28 +0300

    dayliChallengeW4D1Verified.py
    you are printing each letter of the user_input, but it's not what is expected from the question. example: hellohello=> first print 'h' then 'he' then 'hel'...

--- W4/D1/daily_challenge/daily_challenge_W4_D1.py
+++ W4/D1/daily_challenge/daily_challenge_W4_D1.py
@@ -5,7 +5,7 @@ user_input_string = str(input("type a word with 10 characters\n"))
 if len(user_input_string) == 10:
     print("this is the first letter: ",user_input_string[0])
     print("this is the last letter: ", user_input_string[-1])
-    for letter in user_input_string:
+    for letter in user_input_string: # you are printing each letter of the user_input, but it's not what is expected from the question. example: hellohello => first print 'h' then 'he' then 'hel'...
         print(letter)
     print(user_input_string)
 else: 
